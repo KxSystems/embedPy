ba8585b2c062b55175fe9a61b116870fa1a8f552
Conor McCarthy <cmccarthy1@kx.com>
2019-10-23 21:22:47 +0100

    update to 4.7.12 conda for appveyor test

--- build/build.bat
+++ build/build.bat
@@ -14,7 +14,7 @@ set PATH=C:\Miniconda3-x64;C:\Miniconda3-x64\Scripts;%PATH%
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86_amd64
 :: install conda build requirements (use version < 3.12 to avoid warning about verify in output file)
 conda install -y "conda-build<3.12"                                       || goto :error
-conda install -y anaconda-client conda=4.5.4                              || goto :error
+conda install -y anaconda-client conda=4.7.12                             || goto :error
 :: set up kdb+ if available
 if defined QLIC_KC ( echo|set /P=%QLIC_KC% > kc.lic.enc & certutil -decode kc.lic.enc kc.lic & set QLIC=%CD%)
 if "%APPVEYOR_REPO_TAG%"=="true" ( set EMBEDPY_VERSION=%APPVEYOR_REPO_TAG_NAME% )
