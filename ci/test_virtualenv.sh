332c6c1da5779b21813bbd4a1a4d24f0a38115f4
James Hanna <jhanna@kx.com>
2022-12-07 10:46:12 +0000

    remove travis.yml, no longer used

--- ci/test_virtualenv.sh
+++ ci/test_virtualenv.sh
@@ -1,6 +1,7 @@
+P=$1;platform=$2
 pip3 install virtualenv;
 virtualenv test_virtualenv;
 . test_virtualenv/bin/activate
 pip -q install -r tests/requirements.txt;
-q test.q -s 4 -q;
+QHOME=$P/q QLIC=$P $P/q/$platform/q test.q -s 4 -q;
 
